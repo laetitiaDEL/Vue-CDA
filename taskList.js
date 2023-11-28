e17790a762fd4e234bae75cec16d2d55f58002f0
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-28 16:41:19 +0100

    ajout

--- taskList.js
+++ taskList.js
@@ -4,10 +4,24 @@ Vue.createApp({
     //! Toutes les data seront contenues dans this
     data() {
         return {
-        livres: ["Le Hobbit", "Harry Potter et l'Ordre du Phoenix", "Dune"],
-        titrePage: 'Mes Livres'
-    };
+        livres: [{titre: "Le Hobbit", auteur: "J.R.R. Tolkien", annee: 1937}, {titre:"Harry Potter et l'Ordre du Phénix", auteur: "J.K. Rowling", annee: 2003}, {titre: "Dune", auteur: "F. Herbert", annee: 1935}],
+        titrePage: 'Mes Livres',
+        titreLivre: '',
+        auteurLivre: '',
+        anneeLivre: ''
+        };
     },
+
+    methods: {
+        ajouterLivre(){
+            let newLivre = {
+                titre: this.titreLivre,
+                auteur: this.auteurLivre,
+                annee: this.anneeLivre
+            };
+            this.livres.push(newLivre);
+        }
+    }
 
 
     //! L'application est montée sur la balise HTML qui possède l'id app
