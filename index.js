84eb62757b098f6b3c3e07876845a27d80453210
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-11-29 15:47:16 +0100

    bug

--- index.js
+++ index.js
@@ -4,27 +4,36 @@ Vue.createApp({
     //! Toutes les data seront contenues dans this
     data() {
         return {
-            titre: "TP-Event Binding",
-            p1: "",
-            p2: ""
+            titre: "Bug",
+            scale: 1,
+            nombre: 0,
+            user: ""
         };
     },
 
-    methods: {
-        alerte(){
-            alert("T'as tout cassé...");
-        },
-        defP1(event){
-            this.p1 = event.target.value;
-        },
-        defP2(event){
-            this.p2 = event.target.value;
+    computed: {
+        userName(){
+            console.log("fonction executée");
+            if(this.user == ""){
+                return "test";
+            }else{
+                return "un autre test"
+            }
         }
+    },
+
+    methods: {
+        plus(scale){
+            this.nombre += scale;
+        },
+
+        moins(scale){
+            this.nombre -= scale;
+        },
+
     }
 
 
     //! L'application est montée sur la balise HTML qui possède l'id app
     }).mount('#app');
 
-
-    
