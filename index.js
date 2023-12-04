d18b1c480f7d014dc3394ceea8ec41295dd65c41
laetitiaDEL <titia.deloirs@hotmail.fr>
2023-12-04 10:04:32 +0100

    correctif

--- index.js
+++ index.js
@@ -13,17 +13,9 @@ Vue.createApp({
     methods: {
         selectionCard(uneCard){
             if(uneCard == 1){
-                if(this.selectCard1 == false){
-                    this.selectCard1 = true;
-                }else{
-                    this.selectCard1 = false;
-                }
+                this.selectCard1 = !this.selectCard1;
             }else if(uneCard == 2){
-                if(this.selectCard2 == false){
-                    this.selectCard2 = true;
-                }else{
-                    this.selectCard2 = false;
-                }
+                this.selectCard2 = !this.selectCard2;
             }
         }
 
